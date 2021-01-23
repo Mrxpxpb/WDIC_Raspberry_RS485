c435b31c1bbe27633d4b80594b160e11d93b45c3
Mrxpxpb <45600640+Mrxpxpb@users.noreply.github.com>
2021-01-24 00:34:44 +0100

    Fix byte vlaues

--- test_Serial.py
+++ test_Serial.py
@@ -11,8 +11,8 @@ def test_give_adr():
     
 def test_ping():
     assert write_frame(adress = 0, controll= CTRL.PING.value) == "Frame sent"
-    assert ser.readline() == b'\x02\x00\x01\x11\x00\x00\x00\x00\x00\x09\x11\x00\x00\x01\x03\n'
+    assert ser.readline() == b'\x02\x00\x01\x11\x00\x00\x00\x00\x00\x09\x11\x00\x00\x00\x03\n'
     assert write_frame(adress = test_adress, controll= CTRL.PING.value) == "Frame sent"
-    assert ser.readline() == b'\x02\x00\x01\x11\x00\x00\x00\x09\x00\x09\x11\x00\x00\x01\x03\n'
+    assert ser.readline() == b'\x02\x00\x01\x11\x00\x00\x00\x09\x00\x09\x11\x00\x00\x00\x03\n'
     assert write_frame(adress = 69, controll= CTRL.PING.value) == "Frame sent"
     assert ser.readline() == b''
