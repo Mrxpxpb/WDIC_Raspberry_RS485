02e893ccc180560f46f6a92cc5d4dbcc1955d0a2
Mrxpxpb <45600640+Mrxpxpb@users.noreply.github.com>
2021-01-23 23:21:32 +0100

    Add init_serial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WDIC_Raspberry_RS485
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
+![Test Image 1](rs485.png)
---
+++ Serial.py
@@ -0,0 +1,23 @@
+import serial
+import time
+import usb
+import struct
+import enum
+
+vid_pid ="10c4:ea60"
+ser = serial.Serial()
+
+def init_serial():
+    port = usb.get_port(vid_pid)
+    if port != 0:
+        ser.port = port
+        ser.baudrate = 57600
+        ser.bytesize = 8
+        ser.stopbits = 1
+        ser.parity = serial.PARITY_NONE
+        ser.timeout = 5
+        if ser.is_open !=True:
+            ser.open()
+        return "starting"
+    else:
+        return "UART Bridge not found"
