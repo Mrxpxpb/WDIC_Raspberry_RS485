7d55e141cf6dd990a2443ce77209f8d95b606ac0
Mrxpxpb <45600640+Mrxpxpb@users.noreply.github.com>
2021-03-09 08:16:12 +0100

    Add terminal formatting

--- Serial.py
+++ Serial.py
@@ -7,6 +7,15 @@ import enum
 vid_pid ="10c4:ea60"
 ser = serial.Serial()
 
+RED = '\033[91m'
+BOLD = '\033[1m'
+YELLOW = '\033[93m'
+CYAN = '\033[96m'
+GREEN = '\033[92m'
+END = '\033[0m'
+UNDERLINE = '\033[4m'
+buffer = ""
+
 class FRAME(enum.Enum):
     START = 2
     STOP = 3
